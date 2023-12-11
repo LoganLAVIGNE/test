7c07edea3daaf233f33616a814536075d34a5e39
PC Portable <>
2023-12-11 14:26:42 +0100

    Initial commit

---
+++ src/ClassA.php
@@ -0,0 +1,11 @@
+// Fichier : src/ClassA.php
+
+namespace Aperture\Test;
+
+class ClassA
+{
+    public function sayHello()
+    {
+        echo "Hello from ClassA!\n";
+    }
+}
---
+++ src/ClassB.php
@@ -0,0 +1,11 @@
+// Fichier : src/ClassB.php
+
+namespace Aperture\Test;
+
+class ClassB
+{
+    public function sayHello()
+    {
+        echo "Hello from ClassB!\n";
+    }
+}
